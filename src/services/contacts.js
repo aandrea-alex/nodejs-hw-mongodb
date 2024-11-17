381efff49b1794a407a2a08a2cb63deb76a91e5a
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-11-17 17:39:25 +0100

    added try catch

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,27 +1,37 @@
 import Contact from '../models/contact.js';
 
-export const getContacts = async (req, res) => {
-  const contacts = await Contact.find();
+export const getContacts = async (req, res, next) => {
+  try {
+    const contacts = await Contact.find();
 
-  res.send({
-    status: 200,
-    message: 'Successfully found contacts!',
-    data: contacts,
-  });
+    res.send({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getContactById = async (req, res) => {
+export const getContactById = async (req, res, next) => {
   const { id } = req.params;
 
-  const contact = await Contact.findById(id);
+  try {
+    const contact = await Contact.findById(id);
 
-  if (contact === null) {
-    return res.status(404).send({ status: 404, message: 'Contact not found' });
+    if (!contact) {
+      return res.status(404).send({
+        message: 'Contact not found',
+      });
+    }
+
+    res.send({
+      status: 200,
+      message: `Successfully found contact with id ${id}!`,
+      data: contact,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  res.send({
-    status: 200,
-    message: `Successfully found contact with id ${id}!`,
-    data: contact,
-  });
 };
