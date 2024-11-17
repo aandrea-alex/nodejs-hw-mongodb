44080e78fe1e08f9c19a9e05b375f08852836b0f
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-11-17 16:48:01 +0100

    added Contact model

---
+++ src/models/contact.js
@@ -0,0 +1,29 @@
+import { Schema, mongoose } from 'mongoose';
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    phoneNumber: {
+        type: String,
+        required: [true, 'Set phone number'],
+      },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+        type: String,
+        enum: ['work', 'home', 'personal'],
+        required: [true, 'Set contact type'],
+        default: 'personal'
+    }
+  },
+  { versionKey: false, timestamps: true }
+);
+
+export default mongoose.model('Contact', contactSchema);
