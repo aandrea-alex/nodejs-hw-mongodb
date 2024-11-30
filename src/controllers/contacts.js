b47803bd53d019d83adc9a70e493d55e2c8e7696
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-11-30 13:29:09 +0100

    added sorting by keys

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,10 +7,12 @@ import {
   updateContact,
 } from '../services/contacts.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export const getContactsCtrl = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
-  const contacts = await getAllContacts( {page, perPage});
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const contacts = await getAllContacts( {page, perPage, sortBy, sortOrder});
   res.send({
     status: 200,
     message: 'Successfully found contacts!',
