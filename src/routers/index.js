4a044c2c6fefa6d46a7570e0715d445912fef028
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-12-07 16:14:46 +0100

    added user registration

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import contactRouters from './contacts.js';
+import authRouter from './auth.js';
 
 const routers = express.Router();
 
 routers.use('/contacts', contactRouters);
+routers.use('/auth', authRouter);
 
 export default routers;
