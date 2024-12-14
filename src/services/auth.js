6b01b9c15f8cf1978bb40b50f4c11ef0cdcd4088
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-12-14 18:37:13 +0100

    added send email and request-reset-email router

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,12 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { randomBytes } from 'crypto';
 import createHttpError from 'http-errors';
 import { UsersCollection } from '../db/models/user.js';
 import { SessionsCollection } from '../db/models/session.js';
-import { FIFTEEN_MINUTES, ONE_MONTH } from '../constants/index.js';
+import { FIFTEEN_MINUTES, ONE_MONTH, SMTP } from '../constants/index.js';
+import { env } from '../utils/env.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 export const createSession = () => {
     const accessToken = randomBytes(30).toString('base64');
@@ -84,5 +87,23 @@ export const loginUser = async (payload) => {
     if (!user) {
       throw createHttpError(404, 'User not found');
     }
+
+    const resetToken = jwt.sign(
+      {
+        sub: user._id,
+        email,
+      },
+      env('JWT_SECRET'),
+      {
+        expiresIn: '15m',
+      },
+    );
+  
+    await sendEmail({
+      from: env(SMTP.SMTP_FROM),
+      to: email,
+      subject: 'Reset your password',
+      html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+    });
    
   };
