710c6179f237fc2d13f4b2e91e25b05f786f4a57
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-12-21 22:51:24 +0100

    added swagger configuration and docs for getContact by id

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ import routers from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -27,7 +29,9 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cookieParser());
   
-
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+  
   app.use(routers);
 
   app.use('*', notFoundHandler);
